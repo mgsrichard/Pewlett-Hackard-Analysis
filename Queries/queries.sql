0ee65abc7c6e8e2f5d5db732605ed37fa209182f
Martha Richardson <mgsrichard@yahoo.com>
2021-12-30 18:21:02 -0500

    finished modules

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -1,3 +1,4 @@
+
 SELECT first_name,
 	last_name,
 	emp_no,
@@ -60,3 +61,112 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 SELECT * FROM retirement_info
+
+--Joining departments and dept_manager tables
+SELECT d.dept_name,
+	dm.emp_no,
+	dm.from_date,
+	dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+	ON (dm.dept_no = d.dept_no);
+	
+	
+--Joining retirement_info and dept_emp tables to get ees who may already have left
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+	ON ri.emp_no = de.emp_no;
+	
+--Joining retirement_info and dept_emp tables to get only active ees, create new table
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+	ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+--ER Eligible Employee count by department number 
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO er_elig_dept
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+--Employee list (of ER Eligible)
+SELECT e.emp_no,
+	e.last_name,
+	e.first_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+	ON e.emp_no = s.emp_no
+INNER JOIN dept_emp as de
+	ON e.emp_no = de.emp_no
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND(de.to_date = '9999-01-01');
+
+--List of managers by department
+SELECT d.dept_no,
+	d.dept_name,
+	dm.emp_no,
+	ce.last_name,
+	ce.first_name,
+	dm.from_date,
+	dm.to_date
+INTO manager_info
+FROM dept_manager as dm
+INNER JOIN departments as d
+	ON(dm.dept_no = d.dept_no)
+INNER JOIN current_emp as ce
+	ON(dm.emp_no = ce.emp_no);
+	
+--Department Retirees (prospective, not current retirees)
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp as de
+	ON (ce.emp_no = de.emp_no)
+INNER JOIN departments as d
+	ON (de.dept_no = d.dept_no);
+	
+--Sales Department Retirees (prospective, not current retirees)
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO dept_info_sales
+FROM current_emp as ce
+INNER JOIN dept_emp as de
+	ON (ce.emp_no = de.emp_no)
+INNER JOIN departments as d
+	ON (de.dept_no = d.dept_no)
+WHERE d.dept_name = 'Sales';
+
+--Sales/Development Department Retirees (prospective, not current retirees) for mentorship
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+INTO mentorship_info
+FROM current_emp as ce
+INNER JOIN dept_emp as de
+	ON (ce.emp_no = de.emp_no)
+INNER JOIN departments as d
+	ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN ('Sales', 'Development');
