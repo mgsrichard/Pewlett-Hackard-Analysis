6a8bd4459646a47dfe6d26f902429d49e751785b
Martha Richardson <mgsrichard@yahoo.com>
2021-12-30 17:05:16 -0500

    found original error - needed 2 pks in 2 tables

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -1,42 +1,62 @@
-SELECT first_name, last_name, birth_date, emp_no
+SELECT first_name,
+	last_name,
+	emp_no,
+	birth_date
 FROM employees
 WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
 
-SELECT first_name, last_name, birth_date, emp_no
-FROM employees 
-WHERE birth_date BETWEEN '1952-01-01'AND '1952-12-31';
+SELECT first_name,
+	last_name,
+	emp_no,
+	birth_date
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
 
-SELECT first_name, last_name, birth_date, emp_no
-FROM employees 
-WHERE birth_date BETWEEN '1953-01-01'AND '1953-12-31';
+SELECT first_name,
+	last_name,
+	emp_no,
+	birth_date
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
 
-SELECT first_name, last_name, birth_date, emp_no
-FROM employees 
-WHERE birth_date BETWEEN '1954-01-01'AND '1954-12-31';
+SELECT first_name,
+	last_name,
+	emp_no,
+	birth_date
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
 
-SELECT first_name, last_name, birth_date, emp_no
-FROM employees 
-WHERE birth_date BETWEEN '1955-01-01'AND '1955-12-31';
+SELECT first_name,
+	last_name,
+	emp_no,
+	birth_date
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
 
 --Retirement Eligibility
-SELECT first_name, last_name, birth_date, emp_no
+SELECT first_name,
+	last_name,
+	emp_no,
+	birth_date,
+	hire_date
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
-
---Retirement Eligibility Count
+--Count how many are eligible for early retirement - 41380
 SELECT COUNT(first_name)
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
---Export Retirement Eligibility List
-SELECT first_name, last_name, birth_date, emp_no
+SELECT first_name,
+	last_name,
+	emp_no,
+	birth_date,
+	hire_date
 INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
-
-SELECT * FROM retirement_info;
+SELECT * FROM retirement_info
--- schema.sql
+++ schema.sql
@@ -1,10 +1,9 @@
 -- Creating tables for the PH-EmployeeDB
-
 CREATE TABLE departments(
 	dept_no VARCHAR(4) NOT NULL,
 	dept_name VARCHAR(40) NOT NULL,
-	PRIMARY KEY (dept_no),
-	UNIQUE (dept_name)
+	PRIMARY KEY(dept_no),
+	UNIQUE(dept_name)
 );
 
 CREATE TABLE employees(
@@ -14,47 +13,40 @@ CREATE TABLE employees(
 	last_name VARCHAR NOT NULL,
 	gender VARCHAR NOT NULL,
 	hire_date DATE NOT NULL,
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY(emp_no)
 );
 
-CREATE TABLE dept_manager (
-	dept_no VARCHAR(4)NOT NULL,
+CREATE TABLE dept_manager(
+	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no, dept_no)
-);
-
-CREATE TABLE salaries (
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no))
+	
+CREATE TABLE salaries(
 	emp_no INT NOT NULL,
 	salary INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES (emp_no),
-	PRIMARY KEY (emp_no)
-);
-
-CREATE TABLE dept_emp (
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (emp_no));
+	
+CREATE TABLE dept_emp(
 	emp_no INT NOT NULL,
 	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-	PRIMARY KEY (emp_no)
-);
-
-CREATE TABLE titles (
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY (emp_no,dept_no));
+	
+CREATE TABLE titles(
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY(emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, from_date)
 );
-
-SELECT * FROM departments;
